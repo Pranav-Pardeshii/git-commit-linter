e09f325338e2ee2ae80c694fd5af35f21fe147bc
Pranav Pardeshi <im.pranav.pardeshi@gmail.com>
2026-07-06 14:19:35 +0530

    feat: implement commit message validator logic using regex

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from git-commit-linter!")
+import re
 
+def is_valid_commit(message):
+    pattern = r"^(feat|refactor|chore|docs|fix|style|test)(\(.+\))?: .{1, 100}$"
+    return bool(re.match(pattern, message))
 
 if __name__ == "__main__":
-    main()
+    print(is_valid_commit("fixed stuff"))
+    print(is_valid_commit("fix: correct regex"))
+    
