b806ed42742b5d4a2be1b8c49350347ebe599634
Pranav Pardeshi <im.pranav.pardeshi@gmail.com>
2026-07-06 15:09:36 +0530

    feat: add commit-msg hook to validate conventional commit format

---
+++ linter.py
@@ -0,0 +1,24 @@
+import re
+import sys
+
+def main():
+    commit_msg_file = sys.argv[1]
+    with open(commit_msg_file, 'r') as f:
+        message= f.readline().strip()
+    
+    if not is_valid_commit(message):
+        print("The commit message doesn't follow conventional commit message format!")
+        print("eg. 'feat: implement commit message validator using regex'")
+        sys.exit(1)
+    sys.exit(0)
+
+def is_valid_commit(message):
+    pattern = r"^(feat|refactor|chore|docs|fix|style|test)(\(.+\))?: .{1,100}$"
+    return bool(re.match(pattern, message))
+
+if __name__ == "__main__":
+    main()
+
+    # print(is_valid_commit("fixed stuff"))
+    # print(is_valid_commit("fix: correct regex"))
+    
